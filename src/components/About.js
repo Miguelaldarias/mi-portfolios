35bfae64d1e5ade8d2f70ba3fd30f6ee115973fc
Miguel Aldarias <aldarias.88@gmail.com>
2024-12-04 13:05:11 +0100

    About

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,10 @@ const About = () => {
       <div className="container">
         <div className="about-content">
           <h2>Sobre Mí</h2>
-          <p>Soy un programador junior apasionado por crear experiencias digitales atractivas y funcionales, siempre en busca de nuevos retos tecnológicos. Me especializo en el desarrollo tanto de interfaces de usuario dinámicas como en la construcción de backend sólido y eficiente. Disfruto colaborando con equipos para resolver problemas complejos y mejorar la calidad del software, con un enfoque en la mejora continua y el aprendizaje de nuevas tecnologías. Soy una persona orientada a la empresa y no me importa empezar desde abajo para ir escalando dentro de la organización.</p>
+          <p>✔️Soy un programador junior apasionado por crear experiencias digitales atractivas y funcionales, siempre en busca de nuevos retos tecnológicos.</p>
+          <p>✔️Me especializo en el desarrollo tanto de interfaces de usuario dinámicas como en la construcción de backend sólido y eficiente.</p>
+          <p>✔️Disfruto colaborando con equipos para resolver problemas complejos y mejorar la calidad del software, con un enfoque en la mejora continua y el aprendizaje de nuevas tecnologías.</p>
+          <p>✔️Soy una persona orientada a la empresa y no me importa empezar desde abajo para ir escalando dentro de la organización.</p>
           
         </div>
       </div>

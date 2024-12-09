970fb2ecee773b572f3822d1bf2149b81c763913
Miguel Aldarias <aldarias.88@gmail.com>
2024-12-09 19:00:23 +0100

    mejora de enlaces

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,11 +4,8 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="container">
-        <p>&copy; 2024 Juan - Todos los derechos reservados</p>
+        <p>&copy; 2024 Miguel Ángel  - Todos los derechos reservados</p>
         <div className="social-links">
-          <a href="#">LinkedIn</a>
-          <a href="#">GitHub</a>
-          <a href="#">Twitter</a>
         </div>
       </div>
     </footer>

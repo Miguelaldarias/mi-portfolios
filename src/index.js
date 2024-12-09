258fd383c8c988419a63778fa6bdd22fa89e6631
Miguel Aldarias <aldarias.88@gmail.com>
2024-12-09 16:54:18 +0100

    ..,

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import App from './App';
 import './assets/styles/main.sass';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+
+
+
+
+import './assets/styles/main.sass';

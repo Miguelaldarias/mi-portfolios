010856589c0ae2f6fc4ac72f5096a26dd9502f18
Miguel Aldarias <aldarias.88@gmail.com>
2025-02-07 08:41:16 +0100

    proyecto nuevo

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logoImage from '../assets/images/Logo.jpg';
+import logoEscuela from '../assets/images/Logo_esceula_infantil.jpg';
 import '../assets/styles/Projects.sass';
 
 const Projects = () => {
@@ -13,6 +14,11 @@ const Projects = () => {
             <h3>Gymsport</h3>
             <p>Es un proyecto de un gimnasio con todos los comando que puede tener para los clientes y entrenadores. Rutinas, métricas, planes de entrenamiento, planes nutricionales, etc. Tecnologías utilizadas desde el principio: React, HTML, CSS, SASS, JavaScript, Python y MySQL.</p>
             <a href="http://frontend-examen-final.s3-website.eu-north-1.amazonaws.com/" target="_blank" rel="noopener noreferrer" className="btn">Ver Proyecto</a>
+          </div>
+          <div className="project">
+            <img src={logoEscuela} alt="Project 2" />
+            <h3>Escuelas infantiles</h3>
+            <p>Es un sistema integral de gestión para guarderías, diseñado para facilitar la administración de información sobre docentes, alumnos y familiares, con funcionalidades que permiten gestionar registros, horarios, actividades, rutinas diarias y comunicación entre los diferentes actores de la comunidad educativa.</p>
           </div>
         </div>
       </div>

c652136401482f36213b757db744cae3fb0d3a3a
Miguel Aldarias <aldarias.88@gmail.com>
2025-03-28 14:49:41 +0100

    actualización de la formació

--- src/components/Educacion.js
+++ src/components/Educacion.js
@@ -11,7 +11,14 @@ const Educacion = () => {
             <h3>Bootcamp Vive Programando</h3>
             <p><strong>Institución:</strong> Vive Programando</p>
             <p><strong>Duración:</strong> Marzo 2024 - Noviembre 2024</p>
-            <p>Completo aprendizaje de desarrollo web, incluyendo tecnologías como HTML, CSS, JavaScript, React, Python, MySQL, node.js, Express.js, Git y GitHub, PHP.</p>
+            <p>Completo aprendizaje de desarrollo web, incluyendo tecnologías como HTML, CSS, JavaScript, React, Python, MySQL, 
+              node.js, Express.js, Git y GitHub, PHP.</p>
+          </div>
+          <div className="educacion-item">
+            <h3>Desarrolo de Aplicaciones Multiplataforma</h3>
+            <p><strong>Institución:</strong> iFP Innovación y Formación Profesional</p>
+            <p><strong>Duración:</strong> Febrero 2025 - Actualidad</p>
+            <p>Formación profesional como Programador Multiplataforma</p>
           </div>
         </div>
       </div>
